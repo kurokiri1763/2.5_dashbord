49a4619f881dd1aaee2c1f9b9e389afbafab52d1
pachi1376 <kurokiri1763@gmail.com>
2025-05-21 23:14:53 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Admin, Resource, ListGuesser } from "react-admin";
+import { createMuiTheme } from "@material-ui/core/styles";
+import jsonServerProvider from "ra-data-json-server";
+import './App.css';
+
+const theme = createMuiTheme({
+  palette: {
+    type: 'dark',
+  },
+});
+
+//データを取得するクライアントとエンドポイントのベースURLの初期化
+const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
+
+const App: React.FC = () => {
+  return (
+        // dataProviderでクライアントを渡す
+    <Admin theme={theme} dataProvider={dataProvider} >
+      <Resource name="users" list={ListGuesser} />
+      </Admin>
+  );
+};
+export default App;
